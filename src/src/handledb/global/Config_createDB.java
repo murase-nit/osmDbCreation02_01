8113feb3bd77d1db7b88cb67e8d2a5ee061a6542
Masaki Murase <murasemasaki_0216@yahoo.co.jp>
2016-02-24 18:56:22 +0900

    ok

--- src/src/handledb/global/Config_createDB.java
+++ src/src/handledb/global/Config_createDB.java
@@ -3,10 +3,10 @@ package src.handledb.global;
 public class Config_createDB {
 	
 	public static final String DBNAME = "osm_road_db";	// Database Name
-	public static final String SCHEMA = "stroke_v2";
+	public static final String SCHEMA = "stroke_v2";		// スキーマ名.
 	public static final String USER = "postgres";			// user name for DB.
 	public static final String PASS = "usadasql";		// password for DB.
-	public static final String URL = "各自の環境に合わせてDBの設定してください";//"rain2.elcom.nitech.ac.jp";
+	public static final String URL = "各自の環境に合わせてDBの設定してください";//"localhost";rain2.elcom.nitech.ac.jp";
 	public static final int PORT = 5432;
 	public static final String DBURL = "jdbc:postgresql://"+URL+":"+PORT+"/" + DBNAME;
 	
